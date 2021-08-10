74951a775d6d20587eabb906f4df07ea68db70ba
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-08-10 15:43:39 -0700

    Develop model relationships

--- app/models/cell.rb
+++ app/models/cell.rb
@@ -6,4 +6,8 @@ class Cell < ApplicationRecord
 
 	belongs_to :user
 
+	# This is used to link an interior cell to an exterior cell Id, and also give exterior cells list of interiors
+	belongs_to parent:, :class_name => :cell, :foreign_key => "exterior_cell_id"
+	has_many :child_cells, :class_name => :cell, :foreign_key => "exterior_cell_id"
+
 end
