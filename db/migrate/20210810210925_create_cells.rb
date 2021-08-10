1c0749d5d2cac6cd5fc15a90d727b9843e5ba487
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-08-10 14:45:36 -0700

    Edit migration files

--- db/migrate/20210810210925_create_cells.rb
+++ db/migrate/20210810210925_create_cells.rb
@@ -2,19 +2,19 @@ class CreateCells < ActiveRecord::Migration[6.1]
   def change
     create_table :cells do |t|
       t.string :name
+			t.text :description
+			t.integer :priority
       t.integer :ck_coordinate_x
       t.integer :ck_coordinate_y
       t.integer :coordinate_x
       t.integer :coordinate_y
       t.integer :user_id
-      t.text :description
       t.integer :region_id
       t.integer :percent_complete
       t.boolean :interior
       t.integer :exterior_cell_id
-      t.text :color
-      t.integer :priority
-
+      t.string :color
+      
       t.timestamps
     end
   end
