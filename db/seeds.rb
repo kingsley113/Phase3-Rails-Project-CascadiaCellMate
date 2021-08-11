058d31d8043d2d580632003c8348abba3c849212
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-08-10 23:28:56 -0700

    develop seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Regions
+Region.create(name: "North Seattle")
+Region.create(name: "Downtown Seattle")
+Region.create(name: "Ashlands")
+Region.create(name: "Issaquah Alps")
+Region.create(name: "Eastside")
+
+# Users
+
+User.create(username: "kingsley113", password: "12345", display_name: "Cameron")
