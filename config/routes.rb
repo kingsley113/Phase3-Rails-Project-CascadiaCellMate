abfcc8e34780c5d77ed4511ceb3f7c8761c8e0ad
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-08-10 22:22:43 -0700

    Develop routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+	# Cells
+	# Users
+	# Quests
+	# Regions
+	# Comments
+	# Task
+
+	resources :users, only: [:show] do
+		resources :cells, only: [:show, :index] 
+	end
+	
+	resources :cells, :users, :regions, :tasks, :quests 
+
+	root 'welcome#home'
+
+	get '/login' => 'sessions#new'
+	post '/login' => 'sessions#create'
+	post '/logout' => 'sessions#destroy'
+	get '/signup' => 'users#new'
+
 end
