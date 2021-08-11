14edd8bd26829d8765b282e784db1b12b29acb74
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-08-10 22:46:19 -0700

    create tasks controller and index view

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,22 @@
+class TasksController < ApplicationController
+
+	def index
+		#maybe?
+	end
+
+	def new
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update 
+	end
+
+	def destroy
+	end
+
+end
