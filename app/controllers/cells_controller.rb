40a7a3dfe7cf6afb1d56ba872dfc5cb71c453bd8
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-08-11 18:21:21 -0700

    add cell form page

--- app/controllers/cells_controller.rb
+++ app/controllers/cells_controller.rb
@@ -1,7 +1,12 @@
 class CellsController < ApplicationController
 
 	def index
-		@cells = Cell.all
+		if params[:user_id]
+			@user = User.find(params[:user_id])
+			@cells = @user.cells
+		else
+			@cells = Cell.all
+		end
 	end
 
 	def new
