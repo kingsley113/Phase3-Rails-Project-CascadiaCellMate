1690a384420746c42b3c5093b47ec46a75377200
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-08-13 21:26:32 -0700

    develop region show page

--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -1,25 +1,61 @@
 class RegionsController < ApplicationController
 
 	def index
+		@regions = Region.all
 	end
 
 	def new
 		# admin only
+		@region = Region.new
 	end
 
 	def show
+		set_region
+	end
+
+	def create
+		@region = Region.new(region_params)
+
+		if @region.save
+			redirect_to region_path(@region)
+		else
+			# TODO: alert text message
+			render 'new'
+		end
 	end
 
 	def edit
 		# admin only
+		set_region
 	end
 
 	def update 
 		# admin only
+		set_region
+		@region.update(region_params)
+
+		if @region.save
+			redirect_to region_path(@region)
+		else
+			# TODO: alert text message
+			render 'edit'
+		end
 	end
 
 	def destroy
 		# admin only
+		set_region
+		@region.delete
+	end
+
+	private 
+
+	def set_region
+		@region = Region.find(params[:id])
+	end
+
+	def region_params
+		params.require(:region).permit(:name)
 	end
 
 end
