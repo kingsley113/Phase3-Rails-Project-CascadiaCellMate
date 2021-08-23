9fa8fc5bc22b2d17fd09df4a0298aa58171f5dc9
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-08-23 00:18:52 -0700

    Rough out Home Page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,7 @@
 class WelcomeController < ApplicationController
 
 	def home
+		@cells = Cell.all
 		@recent_cells = current_user.get_recent_cells	
 		# @recent_cells = Cell.where(id: current_user.recent_cells)
 	end
